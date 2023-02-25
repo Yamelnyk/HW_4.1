eb37e7543e54d65d86037b5692d7c38522eff262
Yamelnyk <123339195+Yamelnyk@users.noreply.github.com>
2023-02-26 01:17:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const title = document.getElementById('header-two');
+console.log(title);
+
+const section = document.getElementsByTagName('section');
+console.log(section)
+
+const textItem = document.querySelector('li:nth-last-of-type(2)');
+console.log(textItem)
+
+const text = document.getElementsByClassName('hatred-level-block');
+console.log(text);
+
+
+
