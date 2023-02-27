824fbf4d2babebbb8a9dfdaae7bffd2bf36d4960
Yamelnyk <123339195+Yamelnyk@users.noreply.github.com>
2023-02-27 14:55:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const title = document.getElementById('header-two');
 console.log(title);
 
 const section = document.getElementsByTagName('section');
-console.log(section)
+console.log(section[0])
 
 const textItem = document.querySelector('li:nth-last-of-type(2)');
 console.log(textItem)
 
-const text = document.getElementsByClassName('hatred-level-block');
+const text = document.getElementsByClassName('hatred-level-block')[0];
 console.log(text);
+
 
 
 
